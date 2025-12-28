b991f74af5b70d0f0fe8b70512e1179202f975ed
KAVYA <kavyarajput1230@gmail.com>
2025-12-28 09:06:29 +0000

    DAY 1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask backend running on Codespaces"
+
+app.route("/status")
+def status():
+    return {"status": "running"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
